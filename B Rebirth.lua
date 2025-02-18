0a355959b6dd7cc642de549457f8513259e2e202
TheOnlineCat <116917320+TheOnlineCat@users.noreply.github.com>
2025-02-18 23:11:43 +0800

    test

--- B Rebirth.lua
+++ B Rebirth.lua
@@ -443,40 +443,40 @@ do
                 if self._PreviousNPC == boss then continue end --find different target, helpful for timeed out npcs
                 if not table.find(UIController:GetTargetBossNames(), boss:GetAttribute("Name")) then continue end
 
-                local QuestGiver = nil
-                for _, folder in ipairs({NPCsFolder, HiddenNPCsFolder}) do
-                    for _, npc in ipairs(folder:GetChildren()) do
-                        if npc.Name:find(boss.Name) and npc.Name:find("Quest") then
-                            QuestGiver = npc
-                        end
-                    end
-                end
-                if not QuestGiver then
-                    return boss
-                end
+                -- local QuestGiver = nil
+                -- for _, folder in ipairs({NPCsFolder, HiddenNPCsFolder}) do
+                --     for _, npc in ipairs(folder:GetChildren()) do
+                --         if npc.Name:find(boss.Name) and npc.Name:find("Quest") then
+                --             QuestGiver = npc
+                --         end
+                --     end
+                -- end
+                -- if not QuestGiver then
+                --     return boss
+                -- end
 
-                local IsQuestExist = false
-                local timeoutCount = 0
-                repeat
-                    for quest_name, quest_data in pairs(Stats.Quest.Data) do
-                        if quest_data.Type == "Daily" then continue end
-                        if not quest_data.Objectives then continue end
-                        warn(quest_data.Objectives[1].Name, boss.Name, quest_data.Objectives[1].Name == boss.Name)
-                        if quest_data.Objectives[1].Name == boss.Name then 
-                            IsQuestExist = true
-                            break
-                        end
-                    end
+                -- local IsQuestExist = false
+                -- local timeoutCount = 0
+                -- repeat
+                --     for quest_name, quest_data in pairs(Stats.Quest.Data) do
+                --         if quest_data.Type == "Daily" then continue end
+                --         if not quest_data.Objectives then continue end
+                --         warn(quest_data.Objectives[1].Name, boss.Name, quest_data.Objectives[1].Name == boss.Name)
+                --         if quest_data.Objectives[1].Name == boss.Name then 
+                --             IsQuestExist = true
+                --             break
+                --         end
+                --     end
 
-                    if not IsQuestExist then
-                        if timeoutCount >= 2 then
-                            break
-                        end
-                        timeoutCount += 1
-                        self:GetQuest(QuestGiver)
-                        task.wait(0.5)
-                    end 
-                until IsQuestExist
+                --     if not IsQuestExist then
+                --         if timeoutCount >= 2 then
+                --             break
+                --         end
+                --         timeoutCount += 1
+                --         self:GetQuest(QuestGiver)
+                --         task.wait(0.5)
+                --     end 
+                -- until IsQuestExist
 
 
                 return boss
