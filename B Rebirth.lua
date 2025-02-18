a4afaa0f390ad8ccdf02570391a2ea2eff14a490
TheOnlineCat <116917320+TheOnlineCat@users.noreply.github.com>
2025-02-18 23:41:05 +0800

    test

--- B Rebirth.lua
+++ B Rebirth.lua
@@ -993,7 +993,7 @@ do
     
     function UIController:Init()
         local Window = Rayfield:CreateWindow({
-            Name = "Blader's Rebirth v2",
+            Name = "Blader's Rebirth v1",
             LoadingTitle = "Loading User Interface",
             LoadingSubtitle = "Script Credits: OnlineCat",
     
