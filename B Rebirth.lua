0afdb5b0fe1d48f71e47b4c37c5a30fd84b1a8a3
TheOnlineCat <116917320+TheOnlineCat@users.noreply.github.com>
2025-03-03 19:50:50 +0800

    test

--- B Rebirth.lua
+++ B Rebirth.lua
@@ -541,6 +541,7 @@ do
                         for _, npc in ipairs(folder:GetChildren()) do
                             if npc.Name:find(boss.Name) and npc.Name:find("Quest") then
                                 if table.find(UIController:GetCollectedCapsules(), boss.Name) then continue end
+                                UIController:AddCollectedCapsules(UIController:GetCollectedCapsules())
                                 QuestGiver = npc
                             end
                         end
